a49dfe54f94c6c1a35abeec21bbc6062534eac31
Trung Nguyen <trungnth@gmail.com>
2025-03-06 21:53:34 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="akde",
-    version="1.0.0",
+    version="1.0.1",
     author="Trung Nguyen",
     author_email="trungnth@dnri.vn",
     description="Multivariate Adaptive Kernel Density Estimation via Gaussian Mixture Model",
